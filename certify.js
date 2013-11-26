8880dab8a078a5f92e632449365607859cf22a53
Jason Kulatunga <jk17@ualberta.ca>
2013-11-25 22:33:23 -0800

    working first test.

--- certify.js
+++ certify.js
@@ -1,7 +1,36 @@
-var app = angular.module('docsSimpleDirective', [])
+var app = angular.module('simple', [])
 
 app.directive('myCustomer', function() {
     return {
         template: 'Name: {{customer.name}} Address: {{customer.address}}'
     };
 });
+
+
+/*
+Dynamically Enable/Disable Validation::
+
+
+* CreditCard
+  Required
+  Checklist/Array Required
+  MinLength
+  MaxLength
+  Regex
+  Array/ChecklistMinChecked
+  ArrayChecklistMaxChecked
+ Phone Number
+ Remote
+ Date
+ Digits
+ Email
+ EqualTo
+ FileExtensions
+ Integer
+ Max
+ Min
+ Numeric
+ Url/UrlStrict
+ Year
+ AlphaNum
+* */
--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,16 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        //libraries
+        'bower_components/angular/angular.min.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+
+        //files
+        'certify.js',
+
+        //tests
         'test/**/*Spec.js'
+
     ],
 
 
